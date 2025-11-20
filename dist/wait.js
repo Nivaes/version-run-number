f53f8d300c4f32b96dea73e5062bc3f516bb47ee
Andrés Giménez <andres.gimenez@outlook.com>
2025-11-20 12:46:29 +0100

    U`date packages

--- dist/wait.js
+++ dist/wait.js
@@ -1,12 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.wait = void 0;
+exports.wait = wait;
 async function wait(milliseconds) {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
         if (isNaN(milliseconds)) {
             throw new Error("milliseconds not a number");
         }
         setTimeout(() => resolve("done!"), milliseconds);
     });
 }
-exports.wait = wait;
