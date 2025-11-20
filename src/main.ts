99cb1125490f52c169b124afb3b15e139367706b
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2025-11-20 10:56:41 +0100

    Update version-run

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,54 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 
-async function getSubVersion(): Promise<any> {
-    const branchName = github.context.ref_name;
+async function getBranchName(): Promise<string> {
+  const ref = github.context.ref;
+  let branchName = "";
 
-    const token = core.getInput("token", { required: true });
-    const octokit = github.getOctokit(token);
-
-    const owner = github.context.repository.owner;
-    const repo = github.context.repository.repo;
-
-      // Lista TODAS las workflow runs del repo
-    const runs = await octokit.rest.actions.listReleases({
-          owner,
-          repo,
-          per_page: 50 // puedes aumentar de 1 a 100
-      });
-
-      const filtered = releases.data.filter(r =>
-          r.tag_name.startsWith(branchName));
-
-      if(filtered.length == 0) {
-        return 1;
-      } else {
-        return filtered.length + 1;
-      }
+  if (ref.startsWith("refs/heads/")) {
+    branchName = ref.replace(/^refs\/heads\//, "");
+  } else if (ref.startsWith("refs/tags/")) {
+    branchName = ref.replace(/^refs\/tags\//, "");
   }
+  return branchName;
+}
+
+async function getSubVersion(branchName: string): Promise<any> {
+  const token = core.getInput("token", {required: true});
+  const octokit = github.getOctokit(token);
+
+  const {owner, repo} = github.context.repo;
+
+  // Lista TODAS las workflow runs del repo
+  const releases = await octokit.rest.repos.listReleases({
+    owner,
+    repo,
+    per_page: 50 // puedes aumentar de 1 a 100
+  });
+
+  const filtered = releases.data.filter(r => r.tag_name.startsWith(branchName));
+
+  if (filtered.length == 0) {
+    return 1;
+  } else {
+    return filtered.length + 1;
+  }
+}
 
 async function run(): Promise<void> {
   try {
-    const branchName = github.context.ref_name.split("/").pop();
+    const branchName = await getBranchName();
+
     core.debug(`branchName: ${branchName}`);
 
-    let version = `${branchName}`;
-    let subVersion = await getSubVersion(); 
+    let version = branchName.split("/").pop();
+    let subVersion = await getSubVersion(branchName);
 
     core.debug(`Version: ${version}`);
     core.setOutput("version", version);
     core.setOutput("tag-release", `${version}.${subVersion}`);
-    
-  } catch (ex) {
-    core.setFailed(ex.message);
+  } catch (error: any) {
+    core.setFailed(error.message);
   }
 }
 
